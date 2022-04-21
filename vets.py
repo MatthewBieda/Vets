65cfe197e8df194757d4b4e01666be172424f080
MatthewBieda <matthew.bieda@gmail.com>
2022-04-21 16:18:25 +0100

    setup for vets

---
+++ vets.py
@@ -0,0 +1,167 @@
+#Implement animal class-based hierarchy
+#Attributes are only defined in concrete classes
+
+from abc import ABC, abstractmethod
+
+#Class definitions
+
+class Animal(ABC):
+    #Constructor
+    def __init__(self):
+        self.value = "Animal"
+
+    #Methods
+    @abstractmethod
+    def reproduce(self):
+        pass
+
+    @abstractmethod
+    def eat(self):
+        pass
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    def sleep(self):
+        return(f"{self.name} is sleeping")
+
+    def grow(self):
+        return(f"{self.name} is growing larger")
+
+    def die(self):
+        return(f"{self.name} is dying :(")
+
+    def type(self):
+        print(self.value)
+
+class Bird(Animal):
+    #Constructor
+    def __init__(self, wingspan, location, color, name):
+        self.value = "Bird"
+        self.wingspan = wingspan
+        self.location = location
+        self.color = color
+        self.name = name
+
+    #Methods
+    def eat(self):
+        return(f"{self.value} eats worms")
+
+    def move(self):
+        return("I fly")
+    
+    def reproduce(self):
+        return("I lay eggs")
+
+    def type(self):
+        return(self.value)
+
+class Mammal(Animal):
+    #Constructor
+    def __init__(self):
+        self.value = "Mammal"
+
+    #Methods
+    @abstractmethod
+    def eat(self):
+        pass
+
+    def move(self):
+        return("I move on my 4 legs")
+    
+    def reproduce(self):
+        return("I give birth")
+
+    def type(self):
+        return(self.value)
+
+class Fish(Animal):
+    #Constructor
+    def __init__(self):
+        self.value = "Mammal"
+
+    #Methods
+    @abstractmethod
+    def eat(self):
+        pass
+
+    def move(self):
+        return(f"{self.value} swim in the water")
+    
+    def reproduce(self):
+        return("I lay eggs")
+
+    def type(self):
+        return(self.value)
+
+class Cat(Mammal):
+
+    #Constructor
+    def __init__(self, name, age, color):
+        self.value = "Cat"
+        self.name = name
+        self.age = age
+        self.color = color
+
+    #Methods
+    def type(self):
+        return(self.value)
+
+    def eat(self):
+        return(f"{self.name} eats mice")
+
+class Dog(Mammal):
+    #Constructor
+    def __init__(self, name, age, color):
+        self.value = "Dog"
+        self.name = name
+        self.age = age
+        self.color = color
+
+    #Methods
+    def type(self):
+        return(self.value)
+
+    def eat(self):
+        return("I eat dog food")
+
+class AFish(Fish):
+    #Constructor
+    def __init__(self, type, color, region):
+        self.value = "Fish"
+        self.type = type
+        self.color = color
+        self.region = region
+
+    #Methods
+    def type(self):
+        return(self.value)
+
+    def eat(self):
+        return(f"{self.type} eat algae")
+
+    def jump(self):
+        return(f"{self.type} jump out of the sea and dive back in!")
+         
+#Object instantiation 
+
+cat1 = Cat("Allie", 10, "White")
+print(cat1.eat())
+
+dog1 = Dog("Rex", 7, "Red")
+print(dog1.grow())
+print(dog1.die())
+
+Goldie = AFish("Goldfish", "Golden", "Pacific Ocean")
+print(Goldie.move())
+print(Goldie.eat())
+print(f"Fish 1 lives in the {Goldie.region}")
+print(Goldie.jump())
+
+Birdie = Bird("7cm", "North Carolina", "Black", "Blackbird")
+print(Birdie.grow())
+print(Birdie.name)
+
+animal_list = [cat1, dog1, Goldie, Birdie]
+print((animal_list[0].name))
